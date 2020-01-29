bea624cc93f8f3d169cc3dcffd26be66dc7fb3aa
Le Huan <lehuannhatrang98@gmail.com>
2020-01-29 16:55:40 +0700

    clear

--- App/Config/AppConfig.js
+++
@@ -1,8 +0,0 @@
-// Simple React Native specific changes
-
-
-export default {
-  // font scaling override - RN default is on
-  allowTextFontScaling: true,
-  API_BASE_URL: "www.google.com/api/"
-}
--- App/Redux/Reducers/initialState.js
+++
@@ -1,9 +0,0 @@
-/**
- * This is an object that represents the state, it can be a good idea to
- * put it in a separate file so you can reference it easier. It returns the 
- * default products and an empty array as the cart, initialstate for 'cart'
- */
-export default {
-    cart: { cartData: [] },
-    products: []
-}
